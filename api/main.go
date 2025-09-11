33b4dbdeb510455b2946f75fe395f1bc754f6b5c
kiralightyagami <crucisgamer98@gmail.com>
2025-09-11 18:06:59 +0530

    Added: skeleton route

--- api/main.go
+++ api/main.go
@@ -11,5 +11,67 @@ func main() {
 			"message": "Hello World",
 		})
 	})
+
+	router.POST("/users", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "User created",
+		})
+	})
+
+	router.GET("/depth", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Depth fetched",
+		})
+	})
+
+	router.GET("/klines", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Klines fetched",
+		})
+	})
+
+	router.GET("/market", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Market fetched",
+		})
+	})
+
+	router.GET("/tickers", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Tickers fetched",
+		})
+	})
+
+	router.GET("/order", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Orders fetched",
+		})
+	})
+
+	router.POST("/order", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Order executed",
+		})
+	})
+
+	router.DELETE("/order", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Order deleted or canceled",
+		})
+	})
+
+	router.GET("/orders", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Orders fetched",
+		})
+	})
+
+	router.DELETE("/orders", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Order deleted or canceled",
+		})
+	})
+
 	router.Run()
+
 }
